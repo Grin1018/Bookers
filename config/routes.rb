b0a3e24bafc60b3c7e856b6bf5c6dac4e052932b
EC2 Default User <ec2-user@ip-172-31-24-196.ap-northeast-1.compute.internal>
2022-10-17 13:21:11 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+ root to: 'homes#top'
+  get 'books' => 'books#index'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_books'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books 
 end
